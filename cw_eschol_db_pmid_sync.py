78420d994acf057d2d767623cf04d08fdf7d776c
Devin <devin.smith.work@gmail.com>
2026-01-22 14:10:04 -0800

    pmid sync commons swap

--- cw_eschol_db_pmid_sync.py
+++ cw_eschol_db_pmid_sync.py
@@ -1,100 +1,45 @@
-import boto3
-import pymysql
+from pub_oapi_tools_common import eschol_db
+from pub_oapi_tools_common import aws_cloudwatch_logs
 from time import time
 import json
-
-session = boto3.Session()
-verbose = False
 
 
 # =======================================
 def main():
     environments = ['prod']
     for environment in environments:
-        creds = get_creds(environment)
-        pmid_count = get_pmid_count(creds)
-
-        if verbose:
-            print(pmid_count)
-
+        pmid_count = get_pmid_count(environment)
         send_to_cloudwatch(environment, pmid_count)
 
 
 # =======================================
 def send_to_cloudwatch(environment, pmid_count):
-    events_client = session.client(
-        service_name='logs',
-        region_name='us-west-2')
-    log_group = "pub-oapi-tools/eschol-db-monitoring"
-    log_stream = f"pmid-count-{environment}"
-
     timestamp = int(time() * 1000)
     log_entry = json.dumps(pmid_count)
     log_events = [{
         'timestamp': timestamp,
         'message': log_entry}]
 
-    response = events_client.put_log_events(
-        logGroupName=log_group,
-        logStreamName=log_stream,
-        logEvents=log_events)
-
-    if verbose:
-        print(response)
+    aws_cloudwatch_logs.put_logs(
+        log_group="pub-oapi-tools/eschol-db-monitoring",
+        log_stream=f"pmid-count-{environment}",
+        log_events=log_events)
 
 
 # =======================================
 # connect to mysql DB, get queue values
-def get_pmid_count(creds):
-
-    mysql_conn = pymysql.connect(
-        host=creds['server'],
-        user=creds['user'],
-        password=creds['password'],
-        database=creds['database'],
-        cursorclass=pymysql.cursors.DictCursor)
-
-    # Open cursor and send query
+def get_pmid_count(environment):
     query = "select count(id) `eschol_pmid_count` from items " \
             "where attrs->>\"$.local_ids\" like '%pmid%';"
 
-    with mysql_conn.cursor() as cursor:
-        if verbose:
-            print("Connected to eSchol MySQL DB. Sending Query.")
+    # Get connection and send query
+    conn = eschol_db.get_connection(env=environment)
+    with conn.cursor() as cursor:
         cursor.execute(query)
         queue_values = cursor.fetchone()
-
-    mysql_conn.close()
+    conn.close()
 
     return queue_values
-
-
-# =======================================
-# Setup function, connects to AWS for creds
-def get_creds(environment):
-
-    def get_ssm_parameters(folder, names):
-        ssm_client = session.client(
-            service_name='ssm',
-            region_name='us-west-2')
-
-        param_names = [f"{folder}/{name}" for name in names]
-
-        response = ssm_client.get_parameters(
-            Names=param_names,
-            WithDecryption=True)
-
-        param_values = {
-            (param['Name'].split('/')[-1]): param['Value']
-            for param in response['Parameters']}
-
-        return param_values
-
-    creds = get_ssm_parameters(
-        f"/pub-oapi-tools/eschol-db/{environment}",
-        ['user', 'password', 'server', 'database'])
-
-    return creds
 
 
 # =======================================
